a623362fe4ddbb68f0bae8f7198b1b4ba50537fd
K-J-HYEON <kjh950601@naver.com>
2021-12-11 01:10:29 +0900

    Golang-playground

--- for_switch.go
+++ for_switch.go
@@ -3,12 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	for x := 1 ; x <= 15 ;x ++ {
-		if x == 3 {
-			continue
-		} else if x == 8 {
-			break
-		}
-			fmt.Println(x)
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
 	}
+	fmt.Println("done")
 }
